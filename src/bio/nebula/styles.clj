94e5ddc03e9ba9b83347faaa07463c741778e0b8
Ben Sima <bensima@gmail.com>
2015-06-02 12:30:42 -0400

    Fix centering

--- src/bio/nebula/styles.clj
+++ src/bio/nebula/styles.clj
@@ -4,6 +4,7 @@
 
 ;; colors
 (def white "#fff")
+(def light-blue "rgb(114, 140, 191)")
 
 ;; fonts
 (def sans [["1em" "Open Sans"] 'sans-serif])
@@ -11,11 +12,10 @@
 ;; utils
 (defn flex []  {:display "flex"})
 
-
 ;; styles
 (def reset [:html :body {:height "100%"}])
 
-(def body [:body {:background-color "rgb(114, 140, 191)" 
+(def body [:body {:background-color light-blue
                   :color "#fff"}])
 
 (def page [:#page {:width "100%"
@@ -25,15 +25,19 @@
 
 (def content [:#content {:color white
                          :max-width "52em"
+                         :height "100%"
                          :margin "auto"
                          :text-align "center"
                          :display "flex"
                          :flex-direction "column"
+                         :align-items "center"
+                         :justify-content "center"
                          :font sans}])
 
 (def logo [:.logo {:width "298px"
                    :height "272px"
-                   :margin-bottom "4em"}])
+                   :margin-bottom "4em"
+                   :align-self "center"}])
 
 (def links [:#links [:a {:margin "2em 1em"
                          :color white}]])
