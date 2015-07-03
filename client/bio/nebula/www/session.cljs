5e71948358769e2e61ea7faa7a2a73fc51e1862d
Ben Sima <bensima@gmail.com>
2015-07-03 13:20:10 -0400

    Init frontend session handler

---
+++ client/bio/nebula/www/session.cljs
@@ -0,0 +1,17 @@
+(ns bio.nebula.www.session
+  (:refer-clojure :exclude [get])
+  (:requir [reagent.core :as reagent :refer [atom]]))
+
+(def state (atom {}))
+
+(def get [k & [default]]
+  (clojure.core/get @state k default))
+
+(defn put! [k v]
+  (swap! state assoc k v))
+
+(defn update-in! [ks f & args]
+  (clojure.core/swap!
+   state
+   #(apply (partial update-in % ks f) args)))
+
